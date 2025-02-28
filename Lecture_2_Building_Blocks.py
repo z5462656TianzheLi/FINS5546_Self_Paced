28d9db17ff37e1daac290194fbabe64e02999c9c
Tianzhe Li <504046099@qq.com>
2025-03-01 01:05:13 +1100

    Add lecture scripts and update project configuration
    Added Python scripts for foundational programming concepts (Lecture 2: Building Blocks) and control flow (Lecture 3: Control Flow). Updated IntelliJ IDEA project configuration, including CSV editor settings and workspace adjustments. These changes enhance the structure and extend the project with educational materials.

---
+++ Lecture_2_Building_Blocks.py
@@ -0,0 +1,104 @@
+#x = '2103'
+#y = 'he"llo'
+#print(y[1:-3])
+
+#x = 1
+#x = x + 1
+#print(x)
+
+#name = 'SAMLEEtianzhe'
+#name.upper()
+#name1 = name.upper()
+
+#print(name)
+#print(name1)
+#print(id(name))
+#print(id(name1))
+
+#name = 'abdSamleeAbda'
+#print(name.count('a'))
+
+#string3 = '''When Cleese says:
+#    "Now that's what I call a dead parrot"，
+#     so what?'''
+#print(string3)
+
+#f = 1 / 2.000000000000001
+#test = f == 1 / 2
+#print(test)   # --> True
+
+#x = 'abc'
+#xup = x.upper()
+#xup = str.upper(x)
+#print(xup)
+
+#weird_case = "My fUnNy tYpEcAsE sTrInG"
+#weird_case_upper = weird_case.upper
+#print(weird_case_upper())
+
+#a = 5
+#b = True
+#print('I think a is {} and b is {}.'.format(a, b))
+
+#lst = ["string"]
+#lst_2 = ['e','r','g','a']
+#print('The element at index 2 is: "{}", and the element at index 3 is: "{}".'.format(lst[2], lst[3]))
+#print('The element starts from index 1 to index 3 is : {}'.format(lst[1:3]))
+#lst.extend(lst_2)
+#lst.insert(4,'FUCK')
+#print(lst)
+#lst.append(lst_2)
+#print(lst)
+
+#lst.clear()
+#print(lst)
+#lst.sort()
+#print(lst)
+#lst.reverse()
+#print(lst)
+
+#lst.pop()
+#print(lst)
+#lst.remove('r')
+#print(lst)
+
+#print(len(lst))
+#lst.reverse()
+#print(lst)
+
+#tup_1 = (1,2,3,True,"FUCK")
+#A,B,C,D,E = 1,2,3,True,'FUCK'
+#print('The value of A is {}, B is {}, C is {}, D is {}, E is {}'.format(A,B,C,D,E))
+
+#s = set()
+#s.add(1)
+#print(s)
+#s.add(2)
+#s.add(3)
+#print(s)
+#print(1 in s)
+#print('FUCK' in s)
+#print(1 not in s)
+
+#from copy import deepcopy
+#lst1 = ['a']
+#lst2 = ['b',deepcopy(lst1)]
+#lst2[1].append('c')
+#print(lst2)
+#print(lst1)
+
+#lst1 = ['1','2','3']
+#lst2 = ['a', lst1]
+#lst2[1].append('4')
+#print(lst2)
+#print(lst1)
+
+
+
+
+
+
+
+
+
+
---
+++ Lecture_3_Control_Flow.py
@@ -0,0 +1,111 @@
+#happy = True
+#very_happy = True
+#if happy is True:
+#    print('\tI am really happy')
+#    if very_happy is True:
+#        print('\t\tI am really very happy')
+#print('This will be printed regardless of True or False')
+
+#b = False
+#if b is True:
+#    print('b is True')
+#else:
+#    print('b is False')
+
+#x = 1
+#y= 1
+#if x > 0 and y > 0:
+#    pass
+#elif x == 0 and y == 0:
+#    pass
+#else:
+#    pass
+
+#import yfinance as yf
+
+#tic = ['QAN.AX','WES.AX']
+#start_date = '2019-01-01'
+#end_date = None
+
+#for tics in tic:
+#    df = yf.download(tics, start_date, end_date)
+#    tic_base = tics.split('.')[0].lower()
+#    print(tic_base)
+#    print(df)
+    #df.to_csv('{}_stock_price.csv'.format(tic_base))
+
+#d = {
+#    "beauty": True,
+#    "truth": True,
+#    "red wheelbarrow": 100000,
+#    5: "fingers",
+#    }
+
+#for values in d.values():
+    #print(values)
+
+#for pairs in d.items():
+    #print('\n')
+    #print(pairs)
+    #unpacking
+    #key,value = pairs
+    #print('KEY = {}, VALUE = {}'.format(key,value))
+    #direct unpacking
+    #print('KEY = {}, VALUE ={}'.format(*pairs))
+#print('\n')
+#for i in range(5,0,-1):
+#    print('Ignition in {} seconds...'.format(i))
+
+#print('\nIgnite!')
+
+#letters = ["a", "b", "c", "d", "e"]
+#print('Letters = {}'.format(letters))
+#i = 0
+#x = 1.0
+#for letter in letters:
+#    print('Current letter {} = {}_{}'.format(x ,letters[i],i + 1))
+#    i = i + 1
+#    x = x + 1.0
+
+#the_sum = 0
+#i = 0
+#for i in range(0,101,1):
+#    the_sum = the_sum + 1
+#    print('i is now {}, the_sum is now {}'.format(i,the_sum))
+#while i <= 100:
+#    the_sum = the_sum + 1
+#    i = i + 1
+#    print('i is now {}, the_sum is now {}'.format(i,the_sum))
+
+#years = [2018, 2019, 2020]
+#months = [
+#    "Jan",
+#    "Feb",
+#    "Mar",
+#    "Apr",
+#    "May",
+#    "Jun",
+#    "Jul",
+#    "Aug",
+#    "Sep",
+#    "Oct",
+#    "Nov",
+#    "Dec",
+#    ]
+
+#for year in years:
+#    for month in months:
+#        print('It is now {}.{}'.format(year,month))
+
+#sun_of_evens = 0
+#for i in range(0,101,1):
+#    if i % 2 == 0:
+#        continue
+#    if i % 3 == 0:
+#        continue
+#    if i % 7 == 0:
+#        continue
+#    if i % 13  == 0:
+#       continue
+#    sun_of_evens = sun_of_evens + i
+#    print(sun_of_evens)
